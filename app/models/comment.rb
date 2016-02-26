579222000e0e65a4893c2392a2a9590b394e1e29
Jack Brounstein <Jack.Brounstein@yahoo.com>
2016-02-25 16:30:26 -0800

    adds comments to show page

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
 class Comment < ActiveRecord::Base
-  belongs_to :commenter, foreign_key: "user_id", class_name: "User"
-  belongs_to :recipient, foregin_key: "user_id", class_name: "User"
+  belongs_to :commenter, class_name: "User"
+  belongs_to :recipient, class_name: "User"
 end
